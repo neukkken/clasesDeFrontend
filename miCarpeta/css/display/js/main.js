75e44a4e5409302bb3fcf75688963da54dc61b88
neukkken <santyagonarvaezns@gmail.com>
2023-09-06 18:34:52 -0500

    clase de display y tal

---
+++ miCarpeta/css/display/js/main.js
@@ -0,0 +1,9 @@
+let text = document.querySelector(".texto");
+
+function xd(){
+    text.textContent = ("Texto borrao");
+}
+
+function xd2(){
+    text.textContent = ("Eevee (イーブイ Eievui en japonés) es un Pokémon de tipo normal introducido en la primera generación. Se caracteriza por ser el Pokémon con más evoluciones posibles, con ocho. Es el Pokémon inicial en Pokémon: Let's Go, Eevee!.");
+}
